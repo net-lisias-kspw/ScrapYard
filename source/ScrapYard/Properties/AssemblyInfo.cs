cd7bfdce68195fe974b05c72dd3a54709332cbe1
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2023-01-08 22:26:28 -0600

    Update AssemblyInfo.cs
    Co-Authored-By: Lisias <support@lisias.net>

--- source/ScrapYard/Properties/AssemblyInfo.cs
+++ source/ScrapYard/Properties/AssemblyInfo.cs
@@ -2,22 +2,14 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("ScrapYard")]
 [assembly: AssemblyDescription("A common part inventory addon for Kerbal Space Program")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kanford and Sons")]
 [assembly: AssemblyProduct("ScrapYard")]
-[assembly: AssemblyCopyright("Copyright ©  2020, 2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2020, 2023")]
+[assembly: AssemblyTrademark("Trademark ™ 2020, 2023")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6c959482-0901-4325-802c-26702ba75cc7")]
