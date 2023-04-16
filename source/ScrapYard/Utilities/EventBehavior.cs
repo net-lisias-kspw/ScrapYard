332c395f983a6c75dba7d5038811dc2df62e7fcb
Lisias T <me@lisias.net>
2023-04-16 13:08:14 -0300

    Making the thing compilable on my rig

--- source/ScrapYard/Utilities/EventBehavior.cs
+++ source/ScrapYard/Utilities/EventBehavior.cs
@@ -20,11 +20,7 @@
  * THE SOFTWARE.
 */
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace ScrapYard
